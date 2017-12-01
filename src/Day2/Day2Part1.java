10e32ff355ff42f492b1315a9b0212eb7b17fd2c
Greg Pikitis <egblip@gmail.com>
2017-12-01 18:09:04 -0500

    Day 2 folder

---
+++ src/Day2/Day2Part1.java
@@ -0,0 +1,12 @@
+package Day2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day2Part1 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
